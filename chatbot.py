5c6c27f5958e27847436e467ce9291956aa347ea
pcotrim <pcotrimdeabrantes@gmail.com>
2024-03-10 11:08:58 -0300

    up

--- chatbot.py
+++ chatbot.py
@@ -125,7 +125,7 @@ def webhook():
         data = request.json
 
         # Direciona a requisição para o servidor Rasa
-        rasa_url = "http://10.128.0.9:5005/webhooks/rest/webhook"
+        rasa_url = "http://10.128.0.10:5005/webhooks/rest/webhook"
         response = requests.post(rasa_url, json=data)
 
         # Retorna a resposta do servidor Rasa
